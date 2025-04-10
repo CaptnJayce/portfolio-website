fa4db809dc42a8bfcf2b012c0239fea4de8b1746
captnjayce <captn.dev@protonmail.com>
2025-04-10 17:36:06 +0100

    added some pages and a component

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,7 @@
+<div>
+    <button><a href="/">Home</a></button>
+    <button><a href="/about">About</a></button>
+    <button><a href="/projects">Projects</a></button>
+    <button><a href="/games">Soon...</a></button>
+    <button><a href="/links">Links</a></button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+</script>
+
+<Navbar />
+<h1>Welcome to my website... It's a work in progress</h1>
