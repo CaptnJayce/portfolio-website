48558382e45453da322477dce622b12fb0fd8407
CaptnJayce <captn.dev@protonmail.com>
2025-08-18 20:28:59 +0100

    feat: populated expertise section

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,9 +1,35 @@
 export default function Skills() {
     return (
-        <div className="container mx-auto px-8">
+        <div className="container mx-auto px-8 pb-20">
             <div className="flex flex-col justify-center items-center w-full">
                 <div className="w-full max-w-5xl font-[roboto]">
                     <p className="text-[40px] font-[Tiny5]">Expertise</p>
+                    <div className="text-[20px] flex flex-wrap justify-center gap-4">
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            C++
+                        </p>
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            Lua
+                        </p>
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            Linux (Arch BTW)
+                        </p>
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            Raylib
+                        </p>
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            Git
+                        </p>
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            VSCode
+                        </p>
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            Vim
+                        </p>
+                        <p className="border-2 border-[var(--colour-highlight)] rounded-md pr-1 pl-1">
+                            JetBrains
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
